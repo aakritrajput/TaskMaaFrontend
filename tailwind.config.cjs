1b477f3ccb1772003d942e36d409d572ccf079d0
aakritrajput <aakritrajput87@gmail.com>
2025-08-09 19:34:04 +0530

    Initial Setup of the project

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",   // all pages
+    "./components/**/*.{js,ts,jsx,tsx}", // all components
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
