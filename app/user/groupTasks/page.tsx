19979e051da6399b8cda6c51f981507a72bd6e8f
aakritrajput <aakritrajput87@gmail.com>
2025-10-12 11:49:53 +0530

    Added Public group tasks UI in group task page and included options to request in a particular group task. Also updateed the favicon of or app

--- app/user/groupTasks/page.tsx
+++ app/user/groupTasks/page.tsx
@@ -19,6 +19,21 @@ export type groupTaskType = {
     updatedAt?: string;
 }
 
+type publicGroupTaskType = {
+    _id: string;
+    title: string;
+    description?: string;
+    type: 'public';
+    creatorId: {_id: string; profilePic: string; username: string;};
+    dueDate: string;
+    importance: 'low' | 'medium' | 'high' ;
+    status: 'ongoing' | 'completed' ;
+    winners: string[];
+    pendingRequests: string[];
+    createdAt?: string;
+    updatedAt?: string;
+}
+
 export default function GroupTasksPage() {
     const user = useSelector((state: RootState)=> state.auth.user)
 
@@ -57,6 +72,47 @@ export default function GroupTasksPage() {
         type: 'private',
       }
     ]   
+
+    const publicGroupTasks: publicGroupTaskType[] = [
+      {
+        _id: '1',
+        title: '30 Days Coding Challenge',
+        description: 'Complete daily coding challenges to improve problem-solving skills.',
+        importance: 'high',
+        status: 'ongoing',
+        winners: [],
+        creatorId: {_id: 'user_4', profilePic: '/profile/default_profile_pic.png', username: 'aakrit.rajput'}, // here creatorId should contain the populated user's profilepic and user name !!
+        dueDate: '15-10-2025',
+        type: 'public',
+        pendingRequests: ['user1', '12345']
+      },
+      {
+        _id: '2',
+        title: '30 Days Coding Challenge',
+        description: 'Complete daily coding challenges to improve problem-solving skills.',
+        importance: 'high',
+        status: 'ongoing',
+        winners: ['Komal', 'Karan', 'Nina'],
+        creatorId: {_id: '12345', profilePic: '/profile/default_profile_pic.png', username: 'aakrit.rajput'},
+        dueDate: '15-10-2025',
+        type: 'public',
+        pendingRequests: ['user_2'],
+      },
+      {
+        _id: '3',
+        title: 'Challenge',
+        description: 'Complete daily coding challenges to improve problem-solving skills.',
+        importance: 'high',
+        status: 'ongoing',
+        winners: ['Komal', 'Karan', 'Nina'],
+        creatorId: {_id: '12345', profilePic: '/profile/default_profile_pic.png', username: 'aakrit.rajput'},
+        dueDate: '15-10-2025',
+        type: 'public',
+        pendingRequests: [],
+      }
+    ]   
+
+    const requestHandler = () => {}
 
     function ImportanceBadge({ importance }: { importance: groupTaskType["importance"] }) {
         const base = "inline-flex items-center px-2 py-1 rounded-full text-xs font-medium";
@@ -103,6 +159,38 @@ export default function GroupTasksPage() {
           </div>
           {groupTasks.length == 0 && <p className='w-full text-center text-gray-500'>You don&apos;t have any group tasks !!</p>}
         </div>
+
+        <div className='px-2 sm:px-3 border-t-2 border-t-gray-600 md:px-5 lg:px-8 mt-7 py-5'>
+          <h1 className='text-3xl font-bold bg-clip-text bg-gradient-to-br text-transparent from bg-pink-400 to-blue-400'>Public Group Tasks</h1>
+          <div className='mt-5 grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'> {/* Here in future we can implement pagination to load tasks in chunks */}
+            {publicGroupTasks.map(task => (
+              <div key={task._id} className="p-5 rounded-2xl bg-gradient-to-br from-pink-400/30 to-[#00ffff27] backdrop-blur-3xl hover:scale-[1.01] transition border border-white/10">
+                <div className="flex justify-between items-start">
+                  <div>
+                    <h3 className="text-lg font-semibold mb-1">{task.title}</h3>
+                    <p className="text-sm text-white/60 mb-3">{task.description}</p>
+                  </div>
+                  <div className="text-right text-sm">
+                    <span className={`font-semibold ${task.status === 'completed' ? 'text-green-400' : 'text-yellow-300'}`}>{task.status}</span>
+                  </div>
+                </div>
+                <div className="flex justify-between items-center mt-4">
+                  <div>
+                    <p className="text-sm text-white/60">Importance: <ImportanceBadge importance={task.importance}/></p>
+                  </div>
+                </div>
+
+                <div className="flex justify-end items-center mt-4"> {/* Here in pending requests we can add an even listener sought of something that will listen if there is any response like my request is accepted then i should not be shown this gorup task in the suggestions but in my group tasks only !! */}
+                  {/* In future if we have a lot of users then we can here Implement recommendation for the public group tasks and pagination also  !! */}
+                  {/* Also this time we will only allow user to just one time press the btn - that to request and after that he cannot press it like to redo the action as that would complex our first versions making*/}
+                  <button onClick={requestHandler} disabled={task.pendingRequests.includes(user?._id ?? '')} className={`px-3 py-1 text-sm rounded ${task.pendingRequests.includes(user?._id ?? '') ? 'bg-gray-500 cursor-not-allowed' : 'bg-gradient-to-r from-indigo-500 to-teal-400 cursor-pointer'} hover:opacity-90 transition`}>{task.pendingRequests.includes(user?._id ?? '') ? 'Requested...' : 'Request' }</button>
+                </div>
+              </div>
+            ))}
+          </div>
+          {publicGroupTasks.length == 0 && <p className='w-full text-center text-gray-500'>There are not any public group tasks available !!</p>}
+        </div>
+
         {/* Glassmorphism style */}
         <style jsx>{`
           .glass-card {
