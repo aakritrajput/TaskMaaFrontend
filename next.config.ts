54284b59d4e48ab3d373e4399f9c676f440ee5fe
aakritrajput <aakritrajput87@gmail.com>
2025-11-01 14:16:10 +0530

    added configuration for cloudinary  images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "**", // allows all folders under res.cloudinary.com
+      },
+    ],
+  },
 };
 
 export default nextConfig;
