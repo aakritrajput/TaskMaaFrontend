388c6176dcb1601ed8287550089828ff430316ed
aakritrajput <aakritrajput87@gmail.com>
2025-11-06 15:54:17 +0530

    implemented functionality for email verification on frontend

---
+++ app/auth/resendVerificationLink/page.tsx
@@ -0,0 +1,71 @@
+'use client'
+
+import { useForm } from "react-hook-form"
+import { useState } from "react";
+import axios from "axios";
+import Input from "@/src/components/form/Input";
+import Button from "@/src/components/form/Button";
+import { Mail } from "lucide-react";
+import Image from "next/image";
+
+type formData = {
+    email: string;
+}
+
+function ResendVerificationLink() {
+    const {register, handleSubmit, formState: {errors}} = useForm<formData>();
+    const [loading, setLoading] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("")
+    const [successMessage, setSuccessMessage] = useState("");;
+    const submitHandler = async(data: {email: string}) => {
+        setLoading(true);
+        setErrorMessage("");
+        setSuccessMessage("");
+        try {
+            const response = await axios.get(`http://localhost:5000/api/user/resendVerificationLink/${encodeURIComponent(data.email)}`)
+            setSuccessMessage(response.data.message)
+        } catch (error) {
+          console.log('error: ', error)
+            if (axios.isAxiosError(error) && error.response && error.response.data) {
+              setErrorMessage(error.response.data);
+            } else {
+              setErrorMessage("An unexpected error occurred");
+            }
+        }finally{
+            setLoading(false)
+        }
+    }
+  return (
+    <div className="w-full h-[90vh] flex flex-col justify-center items-center bg-transparent">
+        {loading && 
+        <div className="w-full h-[100vh] inset-0 absolute flex justify-center items-center z-20 bg-[#000000c8]">
+            <div className="animate-spin rounded-full h-[8vw] w-[8vw] md:border-t-[6px] border-t-[5px] border-[#20875a]"></div>
+        </div>}
+
+      {/* Logo + Title */}
+        <div className="flex flex-col items-center mb-8">
+          <Image priority src='/TaskMaa_logo.png' alt="TaskMaa logo" width={80} height={80} />
+          <h1 className="text-white text-4xl sm:text-5xl font-extrabold mt-4">TaskMaa</h1>
+        </div>
+        
+      <div className="bg-gradient-to-r from-gray-500/20 to-black/20 shadow-lg shadow-black rounded-2xl flex flex-col gap-3 justify-center items-center relative py-8 md:h-[60%] w-[95%] p-2 md:w-[60%]">
+        <h1 className="text-[#14937e] lg:text-[35px] md:text-[28px] text-[18px] font-semibold w-full h-[30%] text-center top-6">Resend email verification link</h1>
+        <form onSubmit={handleSubmit(submitHandler)} className="m-1 mt-4">
+            <Input
+              type="email"
+              placeholder="Email"
+              icon={<Mail size={18} />}
+              {...register("email", { required: "Email is required" })}
+            />
+            {errors.email && <p className="text-red-400 text-sm">{errors.email.message}</p>}
+            {errors.email && <p className="text-red-600">{errors.email.message}</p>}
+            {errorMessage && <p className="text-red-600">{errorMessage}</p>}
+            {successMessage && <p className="text-green-600">{successMessage}</p>}
+            <Button type="submit" disabled={loading} className="rounded-md my-3 px-4">{loading ? "sending..." : "Send"}</Button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default ResendVerificationLink
---
+++ app/auth/verifyEmail/page.tsx
@@ -0,0 +1,70 @@
+'use client'
+import axios from "axios";
+import Image from "next/image";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState , useCallback} from "react";
+
+
+function VerifyEmail() {
+    
+    const [loading, setLoading] = useState(false);
+    const [successMessage, setSuccessMessage] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+    const searchParams = useSearchParams();
+    const email = searchParams.get("email") || '';
+    const token = searchParams.get("token") || '';
+
+    const verifyEmail = useCallback(async() => {
+        setLoading(true);
+        setErrorMessage("");
+        setSuccessMessage("");
+        const verificationLink = `http://localhost:5000/api/user/register/verify-token?token=${encodeURIComponent(token)}&email=${encodeURIComponent(email)}`;
+        try {
+            const response = await axios.get(verificationLink)
+            setSuccessMessage(response.data.message)
+        } catch (error) {
+            if (axios.isAxiosError(error) && error.response && error.response.data && error.response.data.message) {
+              setErrorMessage(error.response.data.message);
+            } else {
+              setErrorMessage("An unexpected error occurred");
+            }
+        }finally{
+            setLoading(false)
+        }
+    }, [email, token])
+
+    useEffect(()=>{
+        if(email && token){
+            verifyEmail();
+        }else{
+            setErrorMessage("Invalid verification link !!")
+        }
+    }, [email, token, verifyEmail])
+
+  return (
+    <div className="w-full h-[100vh] flex flex-col justify-center items-center bg-transparent">
+        {loading && 
+        <div className="w-full inset-0 h-[100vh] absolute flex justify-center items-center z-20 bg-[#000000c8]">
+            <div className="animate-spin rounded-full h-[10vw] w-[10vw] md:border-t-[6px] border-t-[5px] border-[#118e6c]"></div>
+        </div>}
+      <div className="flex flex-col items-center mb-5">
+        <Image priority src='/TaskMaa_logo.png' alt="TaskMaa logo" width={80} height={80} />
+        <h1 className="text-white text-4xl sm:text-5xl font-extrabold mt-4">TaskMaa</h1>
+      </div>
+      <div className="bg-gradient-to-r from-gray-400/20 to-black/20 shadow-lg shadow-black rounded-2xl flex flex-col  justify-center items-center w-[95%]  py-8 h-[60%] md:w-[60%]">
+       {successMessage && 
+       <div className="flex flex-col justify-center items-center text-[22px] md:text-[32px]">
+
+        <h1 className="text-[23px] md:text-[30px] text-[gray]">Your email is successfully verified !!</h1>
+        <Link href="/auth/login" className="px-3 py-2 rounded-md bg-[#1a9973] text-white">Login</Link>
+       </div>}
+       {errorMessage && <div className="flex flex-col justify-center items-center ">
+        <h1 className="text-[black] font-semibold text-[23px] md:text-[40px]">{errorMessage}</h1>
+        </div>}
+      </div>
+    </div>
+  )
+}
+
+export default VerifyEmail
